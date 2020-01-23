f6a5598de68ac1f16013881e31e568a3089b16e5
saulperez37 <sperez_44@hotmail.com>
2020-01-22 22:15:45 -0700

    added database, schema and routes

---
+++ client/src/utils/API.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const BaseUrl = "https://www.googleapis.com/books/v1/volumes?q="
+
+export default {
+    
+    searchBooks: (query) => axios.get(BaseUrl + query),
+    
+    getBooks: () => axios.get("/api/books"),
+    
+    saveBook: (bookData) => axios.post("/api/books", bookData),
+    
+    deleteBook: (id) => axios.delete("api/books/" + id)
+};
---
+++ controllers/booksController.js
@@ -0,0 +1,49 @@
+const db = require("../models");
+const axios = require("axios");
+
+module.exports = {
+
+    searchApi: (req, res) => {
+
+        axios.get("https://www.googleapis.com/books/v1/volumes?q=flowers")
+            .then((response) => res.json(response.data))
+            .catch(err => res.status(422).json(err))
+
+    },
+
+    findAll: (req, res) => {
+        db.Book
+            .find(req.query)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+    findById: (req, res) => {
+        db.Book
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+
+    save: (req, res) => {
+        db.Book
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+
+    update: (req, res) => {
+        db.Book
+            .findByIdAndUpdate({ _id: req.params.id }, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+
+    remove: (req, res) => {
+        db.Book
+            .findById({ _id: req.params.id })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+
+};
